66d747b2e7c1eb306d66513bed1087cc1ac26f0d
NirajChaudhary143 <nirajchy2000@gmail.com>
2023-07-27 22:26:54 +0545

    temp-and-thumb-product-image-upload

--- app/Http/Controllers/TempImageController.php
+++ app/Http/Controllers/TempImageController.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TempImage;
 use Illuminate\Http\Request;
-
+use Intervention\Image\Facades\Image;
 class TempImageController extends Controller
 {
-    public function store(){
-        
+    public function store(Request $request){
+        if(!empty($request->image)){
+            $image= $request->image;
+
+            $ext =$image->getClientOriginalExtension();
+
+            $tempImg = new TempImage();
+            $tempImg->name = 'null';
+            $tempImg->save();
+
+            $imgName = $tempImg->id.'.'.$ext;
+            $tempImg->name = $imgName;
+            $tempImg->save();
+
+            $image->move(public_path('uploads/temp/'),$imgName);
+
+            // Create Thumbnail
+
+            $sourcePath = public_path('uploads/temp/'.$imgName);
+            $destPath = public_path('uploads/temp/thumb/'.$imgName);
+            $img = Image::make($sourcePath);
+            $img->fit(350,300);
+            $img->save($destPath);
+            return response()->json([
+                'status'=>true,
+                'image_id'=>$tempImg->id,
+                'name'=> $imgName
+            ]);
+        }
     }
 }
